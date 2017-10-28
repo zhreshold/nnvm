570e7bc31fd570b215731d78e98dc0cecb6b3406
Joshua Zhang <cheungchih@gmail.com>
2017-10-27 23:37:39 -0700

    clean cache

--- tutorials/from_onnx.py
+++ tutorials/from_onnx.py
@@ -42,7 +42,7 @@ model_url = ''.join(['https://gist.github.com/zhreshold/',
                      'bcda4716699ac97ea44f791c24310193/raw/',
                      '93672b029103648953c4e5ad3ac3aadf346a4cdc/',
                      'super_resolution_0.2.onnx'])
-download(model_url, 'super_resolution.onnx')
+download(model_url, 'super_resolution.onnx', True)
 # now you have super_resolution.onnx on disk
 onnx_graph = onnx.load('super_resolution.onnx')
 # we can load the graph as NNVM compatible model
