1e9293c718f95d997a65577e96230dab29f51c30
Joshua Zhang <cheungchih@gmail.com>
2017-12-09 17:03:32 -0600

    fix test cases

--- tests/python/frontend/mxnet/test_graph.py
+++ tests/python/frontend/mxnet/test_graph.py
@@ -38,9 +38,9 @@ def test_multi_outputs():
         y = F.sym.Variable('y')
         z = F.sym.split(x, **kwargs)
         return z[0] + z[2] + y
-    mx_sym = compose(mx, num_outputs=3)
+    mx_sym = compose(mx, num_outputs=3, axis=1)
     from_mx_sym, _ = nnvm.frontend.from_mxnet(mx_sym)
-    nnvm_sym = compose(nnvm, indices_or_sections=3)
+    nnvm_sym = compose(nnvm, indices_or_sections=3, axis=1)
     compare_graph(from_mx_sym, nnvm_sym)
 
 if __name__ == '__main__':
